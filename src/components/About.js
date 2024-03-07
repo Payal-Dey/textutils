c6e6a7f3e712233618824c54b02585ecd3fdb00d
Payal Dey <deypayal1996@gmail.com>
2024-03-07 19:27:15 +0530

    dark mode fixed in about js

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function About() {
-  const [myStyle, setMyStyle] = useState({
-    color: "black",
-    backgroundColor: "white",
-  });
-
-  const [btnText, setBtnText] = useState("Enable Dark Mode");
-
-  const toggleStyle = () => {
-    if (myStyle.color === "black") {
-      setMyStyle({
-        color: "white",
-        backgroundColor: "black",
-      });
-      setBtnText("Enable Light Mode");
-    } else {
-      setMyStyle({
-        color: "black",
-        backgroundColor: "white",
-      });
-      setBtnText("Enable Dark Mode");
-    }
-  };
-
+export default function About(props) {
   return (
-    <div className="container" style={myStyle}>
+    <div
+      className="container"
+      style={{
+        backgroundColor: props.mode === "dark" ? "#203354" : "white",
+        color: props.mode === "dark" ? "white" : "black",
+      }}
+    >
       <h1 className="my-2">About Us</h1>
       <div className="accordion" id="accordionExample">
         <div className="accordion-item">
@@ -37,7 +20,10 @@ export default function About() {
               data-bs-target="#collapseOne"
               aria-expanded="true"
               aria-controls="collapseOne"
-              style={myStyle}
+              style={{
+                backgroundColor: props.mode === "dark" ? "#203354" : "white",
+                color: props.mode === "dark" ? "white" : "black",
+              }}
             >
               <strong>About TextUtils</strong>
             </button>
@@ -47,8 +33,26 @@ export default function About() {
             className="accordion-collapse collapse show"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body" style={myStyle}>
-              <strong>TextUtils is a ReactJs website built primarily to do various operations on regular typed text.</strong> You can safely use WordPad or NotePad for text drafting, and saving, but TextUtils offers much more than simple text drafting and formatting. TextUtils can convert your text to any case in just one simple click of a button. You can even remove whitespaces from your scripted documents. It does a detailed analyzing of your text, and provides an output of words, and characters, along with reading time as well. The best part of TextUtils is that it is an open source project, as a result, dozens of new features are in the upcoming, which definitely makes it a cut above the rest.
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "dark" ? "#203354" : "white",
+                color: props.mode === "dark" ? "white" : "black",
+              }}
+            >
+              <strong>
+                TextUtils is a ReactJs website built primarily to do various
+                operations on regular typed text.
+              </strong>{" "}
+              You can safely use WordPad or NotePad for text drafting, and
+              saving, but TextUtils offers much more than simple text drafting
+              and formatting. TextUtils can convert your text to any case in
+              just one simple click of a button. You can even remove whitespaces
+              from your scripted documents. It does a detailed analyzing of your
+              text, and provides an output of words, and characters, along with
+              reading time as well. The best part of TextUtils is that it is an
+              open source project, as a result, dozens of new features are in
+              the upcoming, which definitely makes it a cut above the rest.
             </div>
           </div>
         </div>
@@ -61,7 +65,10 @@ export default function About() {
               data-bs-target="#collapseTwo"
               aria-expanded="false"
               aria-controls="collapseTwo"
-              style={myStyle}
+              style={{
+                backgroundColor: props.mode === "dark" ? "#203354" : "white",
+                color: props.mode === "dark" ? "white" : "black",
+              }}
             >
               <strong>Features of TextUtils</strong>
             </button>
@@ -71,20 +78,30 @@ export default function About() {
             className="accordion-collapse collapse"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body" style={myStyle}>
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "dark" ? "#203354" : "white",
+                color: props.mode === "dark" ? "white" : "black",
+              }}
+            >
               <ul>
-                <li><b>Analyze Your Text: </b> It is a Simple Text Analyzer , which mutilates your text, but in a tender way </li>
-                <li><b>Free To Use: </b>It is completely free to use. No credit cards required. Use as much as you want!!</li>
-                <li><b>Browser Compatible: </b>It is compatible with all of your favorite browsers.</li>
+                <li>
+                  <b>Analyze Your Text: </b> It is a Simple Text Analyzer ,
+                  which mutilates your text, but in a tender way{" "}
+                </li>
+                <li>
+                  <b>Free To Use: </b>It is completely free to use. No credit
+                  cards required. Use as much as you want!!
+                </li>
+                <li>
+                  <b>Browser Compatible: </b>It is compatible with all of your
+                  favorite browsers.
+                </li>
               </ul>
             </div>
           </div>
         </div>
-      </div>
-      <div className="container my-3">
-        <button type="button" className="btn btn-primary" onClick={toggleStyle}>
-          {btnText}
-        </button>
       </div>
     </div>
   );

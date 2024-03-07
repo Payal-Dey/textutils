d812f81d6fd4813a419f29b0f662218e03120567
Payal Dey <deypayal1996@gmail.com>
2024-03-07 18:39:31 +0530

    This is my first textUtils app using react

---
+++ src/components/About.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+
+export default function About() {
+  const [myStyle, setMyStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+
+  const [btnText, setBtnText] = useState("Enable Dark Mode");
+
+  const toggleStyle = () => {
+    if (myStyle.color === "black") {
+      setMyStyle({
+        color: "white",
+        backgroundColor: "black",
+      });
+      setBtnText("Enable Light Mode");
+    } else {
+      setMyStyle({
+        color: "black",
+        backgroundColor: "white",
+      });
+      setBtnText("Enable Dark Mode");
+    }
+  };
+
+  return (
+    <div className="container" style={myStyle}>
+      <h1 className="my-2">About Us</h1>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+              style={myStyle}
+            >
+              <strong>About TextUtils</strong>
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              <strong>TextUtils is a ReactJs website built primarily to do various operations on regular typed text.</strong> You can safely use WordPad or NotePad for text drafting, and saving, but TextUtils offers much more than simple text drafting and formatting. TextUtils can convert your text to any case in just one simple click of a button. You can even remove whitespaces from your scripted documents. It does a detailed analyzing of your text, and provides an output of words, and characters, along with reading time as well. The best part of TextUtils is that it is an open source project, as a result, dozens of new features are in the upcoming, which definitely makes it a cut above the rest.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+              style={myStyle}
+            >
+              <strong>Features of TextUtils</strong>
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              <ul>
+                <li><b>Analyze Your Text: </b> It is a Simple Text Analyzer , which mutilates your text, but in a tender way </li>
+                <li><b>Free To Use: </b>It is completely free to use. No credit cards required. Use as much as you want!!</li>
+                <li><b>Browser Compatible: </b>It is compatible with all of your favorite browsers.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container my-3">
+        <button type="button" className="btn btn-primary" onClick={toggleStyle}>
+          {btnText}
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to Upper Case", "success");
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to Lower Case", "success");
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared", "success");
+  };
+
+  const handleCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Text Copied", "success");
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra Spaces Removed", "success");
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows={8}
+            placeholder="Enter Your Text Here..."
+            style={{
+              backgroundColor: props.mode === "dark" ? "#203354" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+          />
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to Upper Case
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert to Lower Case
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
+        <button className="btn btn-success mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-danger mx-1" onClick={handleClearClick}>
+          Clear Text
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h3>Your Text Summery</h3>
+        <p>
+          <b>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</b> words and <b>{text.length}</b>{" "}
+          characters
+        </p>
+        <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} minutes to read</p>
+        <h3>Preview</h3>
+        <p>
+          {text.length > 0
+            ? text
+            : "Nothing to preview"}
+        </p>
+      </div>
+    </>
+  );
+}
